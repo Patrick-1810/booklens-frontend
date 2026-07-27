fbfa39163614955a4fe9f77b6103818cd04ea7a4
Patrick-1810 <patrickprestes977@gmail.com>
2026-07-27 14:14:24 -0300

    feat(app) install router dom and create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Register } from './pages/Register';
 
 export default function App() {
-    return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
